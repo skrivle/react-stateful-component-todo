769992887e4b145a060927afede8084f712da4f8
Jelle Versele <jelleversele@sparkcentral.com>
2017-12-05 21:50:50 +0100

    add component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {SideEffectProvider} from 'react-stateful-component';
 import './index.css';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<SideEffectProvider><App /></SideEffectProvider>, document.getElementById('root'));
 registerServiceWorker();
