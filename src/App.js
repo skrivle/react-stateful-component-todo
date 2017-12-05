9501e753723542cbd31b0618212c29cb6eb01437
Jelle Versele <jelleversele@sparkcentral.com>
2017-12-05 21:59:14 +0100

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 // @flow
 
 import React from 'react';
-import createStatefulComponent, {update} from 'react-stateful-component';
+import createStatefulComponent, { update } from 'react-stateful-component';
 
 const App = createStatefulComponent(() => ({
-    initialState: () => ({todos: []}),
+    initialState: () => ({ todos: [] }),
     reducer: (state, action) => update.nothing(),
-    render: ({state}) => (<div>todos!</div>)
+    render: ({ state }) => <div>todos!</div>
 }));
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {SideEffectProvider} from 'react-stateful-component';
+import { SideEffectProvider } from 'react-stateful-component';
 import App from './App';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<SideEffectProvider><App /></SideEffectProvider>, document.getElementById('root'));
+ReactDOM.render(
+    <SideEffectProvider>
+        <App />
+    </SideEffectProvider>,
+    document.getElementById('root')
+);
 registerServiceWorker();
